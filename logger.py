78110fdfdc98089f1f5c0efb02bf9442d066a2cd
browolf <brother.wolf@gmail.com>
2022-01-08 13:18:48 +0000

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,17 +1,13 @@
-#super simple logger 
-#usage:
-#import logger
-#logger.log("something happened")
-#logger.closef
-
 import datetime
 import __main__
 
-logfile=open("{f}.log".format(f=__main__.__file__),"a")
+#import logger
+#logger.log("something happened")
+
 
 def log(str):
     timestamp = datetime.datetime.now()
-    logfile.write("{time}: {str}\n".format(time=timestamp.strftime("%Y-%b-%d %H%M"),str=str))
+    with open(f"{__main__.__file__}.log","a") as file:
+        file.write(f"{timestamp.strftime('%Y-%b-%d %H%M')}: {str}\n")
 
-def closef():
-    logfile.close()
+
